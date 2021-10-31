226f60e2897d8d4e9a09fbfa60776e256568d302
issuarez <issuarez@aidacanarias.com>
2021-10-31 11:16:49 +0100

    🔨 Action context "Remove redundant 'else'" on TakeHomeCalculator.cs:42

--- csharp/Hole1/TakeHomeCalculator.cs
+++ csharp/Hole1/TakeHomeCalculator.cs
@@ -39,10 +39,8 @@ namespace Hole1
             {
                 throw new Incalculable();
             }
-            else
-            {
-                return new Pair<int, String>(total.first - tax.first, first.second);
-            }
+
+            return new Pair<int, String>(total.first - tax.first, first.second);
         }
     }
 }
