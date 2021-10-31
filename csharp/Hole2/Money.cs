fad93628e3d71c06eb2972f045c3a383e4a83f73
issuarez <issuarez@aidacanarias.com>
2021-10-31 15:59:12 +0100

    🔨 Replace regex on Money.cs
    Regex "([\s\S]+?)A ([\s\S]+?)B ([\s\S]+?)A ([\s\S]+?)B ([\s\S]+?)"
    to "using System;\n\n$1int $2String $3int $4String $5" on Money.cs.
    Reformat indentation.

--- csharp/Hole2/Money.cs
+++ csharp/Hole2/Money.cs
@@ -1,11 +1,13 @@
-﻿namespace Hole2
+﻿using System;
+
+namespace Hole2
 {
     public class Money<A, B>
     {
-        public readonly A first;
-        public readonly B second;
+        public readonly int first;
+        public readonly String second;
 
-        public Money(A first, B second)
+        public Money(int first, String second)
         {
             this.first = first;
             this.second = second;
